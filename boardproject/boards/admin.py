84f2626e2a95b60eb4a58412808b3ae4618e125c
Umesh <umeshtamang129@gmail.com>
2025-01-02 11:24:23 +0545

    customize admin for Board model

--- boardproject/boards/admin.py
+++ boardproject/boards/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Board
 
 # Register your models here.
+class BoardAdmin(admin.ModelAdmin):
+    list_display=["name","description"]
+    list_display_links=["name","description"]        
+
+admin.site.register(Board,BoardAdmin)
+
