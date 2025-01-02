754212421bb989cb14d8e8497e860f14ad504b91
Umesh <umeshtamang129@gmail.com>
2025-01-02 11:54:12 +0545

    templete setup

--- boardproject/boards/views.py
+++ boardproject/boards/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello board project")
+    boards=Board.objects.all()
+    context={
+        "boards":boards
+    }
+    return render(request,"home.html",context)
 
